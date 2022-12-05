7670ea98776951f75b8a2b5f68242b34bd3d5878
DavidTorresOvallos <davidtorresovallos@gmail.com>
2022-12-04 19:15:34 -0500

    Subiendo Base

---
+++ src/Product.js
@@ -0,0 +1,11 @@
+function Product(){
+    return <div>
+            <h1>Producto</h1>
+    </div>
+}
+
+export function Navbar(){
+    return <h1>Hola soy un navegador</h1>
+}
+
+export default Product;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {Greeting, UserCard } from "./Greeting";
+import Product, {Navbar}from "./Product";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Greeting title = "Hola Mundo" name = "Bryan" />
+    <Greeting title = "Hola React" name = "Toretto" />
+    <Greeting title = "Si te pica es mi culebrita"/>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
